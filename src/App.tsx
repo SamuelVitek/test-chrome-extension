1763378a98e25ec2c20b3056991df686fbf88fbc
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2023-05-19 21:25:50 +0200

    Setting up the test project (somehow) so I can type in the input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <header>
-        <h5>Hello From React App</h5>
-      </header>
-    </div>
-  );
+    const [url, setUrl] = useState('');
+
+    // @ts-ignore
+    const test = () => {
+
+        // @ts-ignore
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            const activeTabId = tabs[0].id
+
+            // @ts-ignore
+            chrome.scripting.executeScript(
+                {
+                    target: {tabId: activeTabId},
+                    // @ts-ignore
+                    function: (url) => {
+                        console.log(url);
+                    },
+                    args: [url],
+                }
+            )
+        })
+    }
+
+    test();
+
+    return (
+        <div>
+            <input
+                type="text"
+                id="message"
+                name="message"
+                onChange={event => setUrl(event.target.value)}
+            />
+        </div>
+    );
 }
 
 export default App;
